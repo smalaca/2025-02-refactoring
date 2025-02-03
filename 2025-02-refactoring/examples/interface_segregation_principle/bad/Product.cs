6b8f5c23d07a6d91ecf026d1aeeeb9322a441aea
smalaca <ab13.krakow@gmail.com>
2025-02-03 17:31:52 +0100

    example of Interface Segregation Principle

---
+++ 2025-02-refactoring/examples/interface_segregation_principle/bad/Product.cs
@@ -0,0 +1,25 @@
+﻿namespace _2025_02_refactoring.examples.interface_segregation_principle.bad;
+
+public interface IProductPurchase
+{
+    void Purchase();
+    void Refund();
+    void TrackInventory(); // Not relevant for all product types
+    void GenerateSalesReport(); // Not relevant for all product types
+}
+
+public class PhysicalProduct : IProductPurchase
+{
+    public void Purchase() { /* ... */ }
+    public void Refund() { /* ... */ }
+    public void TrackInventory() { /* ... */ }
+    public void GenerateSalesReport() { /* ... */ }
+}
+
+public class DigitalProduct : IProductPurchase
+{
+    public void Purchase() { /* ... */ }
+    public void Refund() { /* ... */ }
+    public void TrackInventory() { /*  Throws NotImplementedException - not applicable */ } // Problem!
+    public void GenerateSalesReport() { /* Throws NotImplementedException - not applicable */ } // Problem!
+}
---
+++ 2025-02-refactoring/examples/interface_segregation_principle/good/Product.cs
@@ -0,0 +1,32 @@
+﻿namespace _2025_02_refactoring.examples.interface_segregation_principle.good;
+
+public class PhysicalProduct : IPurchasable, IInventoryTrackable, IReportable
+{
+    public void Purchase() { /* ... */ }
+    public void Refund() { /* ... */ }
+    public void TrackInventory() { /* ... */ }
+    public void GenerateSalesReport() { /* ... */ }
+}
+
+public class DigitalProduct : IPurchasable
+{
+    public void Purchase() { /* ... */ }
+    public void Refund() { /* ... */ }
+    // No need to implement IInventoryTrackable or IReportable
+}
+
+public interface IPurchasable
+{
+    void Purchase();
+    void Refund();
+}
+
+public interface IInventoryTrackable
+{
+    void TrackInventory();
+}
+
+public interface IReportable
+{
+    void GenerateSalesReport();
+}
