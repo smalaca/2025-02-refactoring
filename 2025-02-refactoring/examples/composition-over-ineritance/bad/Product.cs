df8e22c4b1823883c6b198e904f3985ae08e745c
smalaca <ab13.krakow@gmail.com>
2025-02-03 16:35:33 +0100

    add examples of composition over inheritance

---
+++ 2025-02-refactoring/examples/composition-over-ineritance/bad/Product.cs
@@ -0,0 +1,44 @@
+﻿namespace _2025_02_refactoring.examples.composition_over_ineritance.bad;
+
+public class Product
+{
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+
+    public virtual string GetProductDetails()
+    {
+        return $"Name: {Name}, Price: {Price}";
+    }
+}
+
+public class DiscountedProduct : Product
+{
+    public decimal DiscountPercentage { get; set; }
+
+    public override string GetProductDetails()
+    {
+        return $"{base.GetProductDetails()}, Discount: {DiscountPercentage}%";
+    }
+
+    public decimal GetDiscountedPrice()
+    {
+        return Price * (1 - DiscountPercentage / 100);
+    }
+}
+
+public class FreeShippingProduct : Product
+{
+    public override string GetProductDetails()
+    {
+        return $"{base.GetProductDetails()}, Free Shipping";
+    }
+}
+
+// ?????
+public class DiscountedFreeShippingProduct : DiscountedProduct 
+{
+    public override string GetProductDetails()
+    {
+        return $"{base.GetProductDetails()}, Free Shipping"; 
+    }
+}
---
+++ 2025-02-refactoring/examples/composition-over-ineritance/good/Product.cs
@@ -0,0 +1,76 @@
+﻿namespace _2025_02_refactoring.examples.composition_over_ineritance.good;
+
+public class Discount
+{
+    public decimal Percentage { get; set; }
+
+    public decimal ApplyDiscount(decimal price)
+    {
+        return price * (1 - Percentage / 100);
+    }
+
+    public string GetDiscountDetails()
+    {
+        return $"Discount: {Percentage}%";
+    }
+}
+
+public class FreeShipping
+{
+    public string GetFreeShippingDetails()
+    {
+        return "Free Shipping";
+    }
+}
+
+
+public class Product
+{
+    private Discount _discount;
+    private FreeShipping _freeShipping;
+    private string _name;
+    private decimal _price;
+
+    public Product(string name, decimal price)
+    {
+        _name = name;
+        _price = price;
+    }
+
+    public void AddDiscount(Discount discount)
+    {
+        _discount = discount;
+    }
+
+    public void AddFreeShipping(FreeShipping freeShipping)
+    {
+        _freeShipping = freeShipping;
+    }
+
+    public string GetProductDetails()
+    {
+        string details = $"Name: {_name}, Price: {_price}";
+
+        if (_discount != null)
+        {
+            details = $"{details}, {_discount.GetDiscountDetails()}";
+        }
+
+        if (_freeShipping != null)
+        {
+            details = $"{details}, {_freeShipping.GetFreeShippingDetails()}";
+        }
+
+        return details;
+    }
+
+    public decimal GetPrice()
+    {
+        if (_discount != null)
+        {
+            return _discount.ApplyDiscount(_price);
+        }
+        
+        return _price;
+    }
+}
