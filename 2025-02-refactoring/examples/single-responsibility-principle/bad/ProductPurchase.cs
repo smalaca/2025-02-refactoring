27d142a3ad6def753e7fb4a6f2e0663c5ce655c8
smalaca <ab13.krakow@gmail.com>
2025-02-03 16:55:41 +0100

    example of SRP

---
+++ 2025-02-refactoring/examples/single-responsibility-principle/bad/ProductPurchase.cs
@@ -0,0 +1,26 @@
+﻿namespace _2025_02_refactoring.examples.single_responsibility_principle.bad;
+
+public class ProductPurchase
+{
+    public int ProductId { get; set; }
+    public int Quantity { get; set; }
+    public string CustomerEmail { get; set; } 
+
+    public void PurchaseProduct()
+    {
+        if (!CheckProductAvailability(ProductId, Quantity))
+        {
+            throw new Exception("Product not available.");
+        }
+
+        ProcessPayment(CustomerEmail, CalculateTotal());
+        UpdateInventory(ProductId, Quantity);
+        SendConfirmationEmail(CustomerEmail, ProductId, Quantity);
+    }
+
+    private bool CheckProductAvailability(int productId, int quantity) { /* ... */  return true; }
+    private void ProcessPayment(string customerEmail, decimal total) { /* ... */ }
+    private void UpdateInventory(int productId, int quantity) { /* ... */ }
+    private void SendConfirmationEmail(string customerEmail, int productId, int quantity) { /* ... */ }
+    private decimal CalculateTotal() { /* ... */ return 10; }
+}
---
+++ 2025-02-refactoring/examples/single-responsibility-principle/good/ProductPurchase.cs
@@ -0,0 +1,51 @@
+﻿namespace _2025_02_refactoring.examples.single_responsibility_principle.good;
+
+public class ProductPurchase
+{
+    public int ProductId { get; set; }
+    public int Quantity { get; set; }
+    public string CustomerEmail { get; set; }
+
+    private readonly InventoryService _inventoryService;
+    private readonly PaymentService _paymentService;
+    private readonly ConfirmationService _confirmationService; // For email
+
+    public ProductPurchase(InventoryService inventoryService, PaymentService paymentService, ConfirmationService confirmationService)
+    {
+        _inventoryService = inventoryService;
+        _paymentService = paymentService;
+        _confirmationService = confirmationService;
+    }
+
+
+    public void CreateOrder()
+    {
+        if (!_inventoryService.CheckProductAvailability(ProductId, Quantity))
+        {
+            throw new Exception("Product not available.");
+        }
+
+        _paymentService.ProcessPayment(CustomerEmail, CalculateTotal());
+        _inventoryService.UpdateInventory(ProductId, Quantity);
+        _confirmationService.SendConfirmationEmail(CustomerEmail, ProductId, Quantity);
+    }
+
+    private decimal CalculateTotal() { /* ... */ return 10; }
+}
+
+public class InventoryService
+{
+    public bool CheckProductAvailability(int productId, int quantity) { /* ... */ return true; }
+    public void UpdateInventory(int productId, int quantity) { /* ... */ }
+}
+
+public class PaymentService
+{
+    public void ProcessPayment(string customerEmail, decimal total) { /* ... */ }
+}
+
+public class ConfirmationService
+{
+    public void SendConfirmationEmail(string customerEmail, int productId, int quantity) { /* ... */ }
+}
+
